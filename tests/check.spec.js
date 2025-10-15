d1f896d35d35ba128eb78fcdef79700e1073a549
Your Name <you@example.com>
2025-10-16 01:12:26 +0530

    run playwright

--- tests/check.spec.js
+++ tests/check.spec.js
@@ -1,7 +1,8 @@
 import { test } from '@playwright/test';
+import { BASE_URL } from '../utils/env';
 
 
 test('test', async ({ browser }) => {
     console.log("Success CI CD");
-    
+    console.log(BASE_URL)
 })
