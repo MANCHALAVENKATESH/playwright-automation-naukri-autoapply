a696b35e5aec16f0d84518033f1d0827e4c1511a
Your Name <you@example.com>
2025-10-17 00:38:12 +0530

    run playwright naukriLoginPipeLine

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,9 @@ export default defineConfig({
     headless: true,                 // set true for CI
     browserName: 'chromium',         // chromium, firefox, or webkit
     viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true, // ignore HTTPS errors in CI
+    locale: 'en-US', // set locale
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
